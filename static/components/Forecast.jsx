39da66570da3ef1859bc5d2a84ffc6080096c37f
Sajo Sam <66501284+sajosam@users.noreply.github.com>
2026-04-26 17:40:49 +0530

    feat: add static/components/Forecast.jsx [9/13]

---
+++ static/components/Forecast.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forecast = () => {
+  return (
+    <div>
+      <h2>Forecast</h2>
+      <p>Forecast data will be displayed here</p>
+    </div>
+  );
+};
+
+export default Forecast;
