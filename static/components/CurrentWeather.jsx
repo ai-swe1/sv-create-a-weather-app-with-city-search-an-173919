2e831c720e5a620a2394564098672061631c65e7
Sajo Sam <66501284+sajosam@users.noreply.github.com>
2026-04-26 17:40:48 +0530

    feat: add static/components/CurrentWeather.jsx [8/13]

---
+++ static/components/CurrentWeather.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CurrentWeather = () => {
+  return (
+    <div>
+      <h2>Current Weather</h2>
+      <p>Temperature: </p>
+      <p>Windspeed: </p>
+      <p>Weathercode: </p>
+    </div>
+  );
+};
+
+export default CurrentWeather;
